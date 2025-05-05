4c9fd41c5c5969a2f89ba285630b6e11618bd1dc
Valeriia <valeriia.vp1@yandex.ru>
2025-05-05 19:14:40 +0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+
+namespace MobileApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+           // LoadImage();
+        }
+
+        private async Task LoadImage()
+        {
+           /* try
+            {
+                var client = new HttpClient();
+                var response = await client.GetAsync("https://th.bing.com/th/id/OIP.CDeAgNnAG-yyCgEXPq9dhwHaHa?rs=1&pid=ImgDetMain");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var imageBytes = await response.Content.ReadAsByteArrayAsync();
+                    var imageSource = ImageSource.FromStream(() => new System.IO.MemoryStream(imageBytes));
+                    imageControl.Source = imageSource;
+
+                    messageLabel.Text = "Изображение загружено";
+                }
+                else
+                {
+                    messageLabel.Text = "Ошибка при загрузке изображения";
+                }
+            }
+            catch (Exception ex)
+            {
+                messageLabel.Text = $"Ошибка: {ex.Message}";
+            } */
+
+
+
+            /*try
+            {
+                var client = new HttpClient();
+                var response = await client.GetAsync("https://th.bing.com/th/id/OIP.CDeAgNnAG-yyCgEXPq9dhwHaHa?rs=1&pid=ImgDetMain");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var imageBytes = await response.Content.ReadAsByteArrayAsync();
+                    var imageSource = ImageSource.FromStream(() => new System.IO.MemoryStream(imageBytes));
+
+                    // Присваиваем изображение контролу Image
+                    imageControl.Source = imageSource;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка при загрузке изображения");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }*/
+        }
+    }
+    
+
+}
